85ec5212020f3eca306682fb1964072637788a46
Владимир Журавлев <vzhuravlyov@astralinux.ru>
2023-04-05 12:42:36 +0300

    update repo

---
+++ src/extension.ts
@@ -0,0 +1,54 @@
+import { ExtensionContext } from 'vscode';
+import { ACSLCompletionItemProvider, 
+    ACSLDocumentFormattingEditProvider, 
+    CCompletionItemProvider } from '../language-features/acsl';
+import * as vscode from 'vscode';
+
+
+
+const completionProvider = new ACSLCompletionItemProvider();
+export function activate(context: ExtensionContext) {
+    context.subscriptions.push(
+        vscode.languages.registerCompletionItemProvider(
+            { scheme: 'file', language: 'c' },
+            completionProvider,
+            '@'
+        ),
+        
+    );
+    context.subscriptions.push(
+        vscode.languages.registerDocumentFormattingEditProvider(
+            'c', new ACSLDocumentFormattingEditProvider()
+        )
+    );
+    context.subscriptions.push(
+        vscode.languages.registerCompletionItemProvider(
+            'c',
+            new CCompletionItemProvider()
+        )
+    );
+}
+
+export function openRecentFiles() {
+    const config = vscode.workspace.getConfiguration('acsl-astra-c-extension');
+    const maxHistory = config.get<number>('maxRecentFiles') || 5;
+  
+    vscode.commands.executeCommand('workbench.action.openRecent');
+    
+    // Get the "Open Recently Used" editor
+    const openEditors = vscode.window.visibleTextEditors.filter(
+      editor => editor.document.uri.scheme === 'recentlyUsed'
+    );
+  
+    // Limit the number of recently used files to `maxHistory`
+    const recentFiles = openEditors.slice(0, maxHistory);
+  
+    // Show the quick pick menu to open the selected file
+    vscode.window.showQuickPick(recentFiles.map(editor => editor.document.fileName)).then(filePath => {
+      if (filePath) {
+        vscode.workspace.openTextDocument(filePath).then(doc => {
+          vscode.window.showTextDocument(doc);
+        });
+      }
+    });
+  }
