13d1aa3057e3c82b7907895594d64aad10de932f
Владимир Журавлев <vzhuravlyov@astralinux.ru>
2023-04-18 01:57:55 +0300

    update extension

--- out/extension.js
+++ out/extension.js
@@ -1,36 +1,41 @@
-// import { ExtensionContext } from 'vscode';
-// import * as vscode from 'vscode';
-// export function openRecentFiles() {
-//     const config = vscode.workspace.getConfiguration('acsl-astra-c-extension');
-//     const maxHistory = config.get<number>('maxRecentFiles') || 5;
-//     vscode.commands.executeCommand('workbench.action.openRecent');
-//     // Get the "Open Recently Used" editor
-//     const openEditors = vscode.window.visibleTextEditors.filter(
-//       editor => editor.document.uri.scheme === 'recentlyUsed'
-//     );
-//     // Limit the number of recently used files to `maxHistory`
-//     const recentFiles = openEditors.slice(0, maxHistory);
-//     // Show the quick pick menu to open the selected file
-//     vscode.window.showQuickPick(recentFiles.map(editor => editor.document.fileName)).then(filePath => {
-//       if (filePath) {
-//         vscode.workspace.openTextDocument(filePath).then(doc => {
-//           vscode.window.showTextDocument(doc);
-//         });
-//       }
-//     });
-//   }
-// export function activate(context: ExtensionContext) {
-//     context.subscriptions.push(vscode.commands.registerCommand('acsl-astra-c-extension.openRecentFiles', () => {
-//         openRecentFiles();
-//     }));
-// }
-
-
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import * as vscode from 'vscode';
+// The extension deactivate method is asynchronous, so we handle the disposables ourselves instead of using extensionContext.subscriptions.
+const disposables = [];
+export function initialize(context) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // Register DebugConfigurationProviders for "Run and Debug" play button.
+        disposables.push(vscode.commands.registerTextEditorCommand("acsl-astra-c-extension.AddDebugConfiguration", (textEditor, edit, ...args) => __awaiter(this, void 0, void 0, function* () {
+        })));
+    });
+}
+export function openRecentFiles() {
+    const config = vscode.workspace.getConfiguration('acsl-astra-c-extension');
+    const maxHistory = config.get('maxRecentFiles') || 5;
+    vscode.commands.executeCommand('workbench.action.openRecent');
+    // Get the "Open Recently Used" editor
+    const openEditors = vscode.window.visibleTextEditors.filter(editor => editor.document.uri.scheme === 'recentlyUsed');
+    // Limit the number of recently used files to `maxHistory`
+    const recentFiles = openEditors.slice(0, maxHistory);
+    // Show the quick pick menu to open the selected file
+    vscode.window.showQuickPick(recentFiles.map(editor => editor.document.fileName)).then(filePath => {
+        if (filePath) {
+            vscode.workspace.openTextDocument(filePath).then(doc => {
+                vscode.window.showTextDocument(doc);
+            });
+        }
+    });
+}
 export function activate(context) {
-    const command = 'acsl-astra-c-extension.sayHello';
-    const commandHandler = (name = 'world') => {
-        console.log(`Hello ${name}!!!`);
-    };
-    context.subscriptions.push(vscode.commands.registerCommand(command, commandHandler));
+    context.subscriptions.push(vscode.commands.registerCommand('acsl-astra-c-extension.openRecentFiles', () => {
+        openRecentFiles();
+    }));
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,45 +1,53 @@
-// import { ExtensionContext } from 'vscode';
-// import * as vscode from 'vscode';
-
-// export function openRecentFiles() {
-//     const config = vscode.workspace.getConfiguration('acsl-astra-c-extension');
-//     const maxHistory = config.get<number>('maxRecentFiles') || 5;
-  
-//     vscode.commands.executeCommand('workbench.action.openRecent');
-    
-//     // Get the "Open Recently Used" editor
-//     const openEditors = vscode.window.visibleTextEditors.filter(
-//       editor => editor.document.uri.scheme === 'recentlyUsed'
-//     );
-  
-//     // Limit the number of recently used files to `maxHistory`
-//     const recentFiles = openEditors.slice(0, maxHistory);
-  
-//     // Show the quick pick menu to open the selected file
-//     vscode.window.showQuickPick(recentFiles.map(editor => editor.document.fileName)).then(filePath => {
-//       if (filePath) {
-//         vscode.workspace.openTextDocument(filePath).then(doc => {
-//           vscode.window.showTextDocument(doc);
-//         });
-//       }
-//     });
-//   }
-
-
-// export function activate(context: ExtensionContext) {
-//     context.subscriptions.push(vscode.commands.registerCommand('acsl-astra-c-extension.openRecentFiles', () => {
-//         openRecentFiles();
-//     }));
-// }
+import { ExtensionContext } from 'vscode';
 
 import * as vscode from 'vscode';
+import * as os from 'os';
 
-export function activate(context: vscode.ExtensionContext) {
-  const command = 'acsl-astra-c-extension.sayHello';
 
-  const commandHandler = (name: string = 'world') => {
-    console.log(`Hello ${name}!!!`);
-  };
 
-  context.subscriptions.push(vscode.commands.registerCommand(command, commandHandler));
+// The extension deactivate method is asynchronous, so we handle the disposables ourselves instead of using extensionContext.subscriptions.
+const disposables: vscode.Disposable[] = [];
+
+
+
+export async function initialize(context: vscode.ExtensionContext): Promise<void> {
+
+    // Register DebugConfigurationProviders for "Run and Debug" play button.
+
+    disposables.push(vscode.commands.registerTextEditorCommand("acsl-astra-c-extension.AddDebugConfiguration", async (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]) => {
+    }));
+
+
 }
+
+export function openRecentFiles() {
+    const config = vscode.workspace.getConfiguration('acsl-astra-c-extension');
+    const maxHistory = config.get<number>('maxRecentFiles') || 5;
+  
+    vscode.commands.executeCommand('workbench.action.openRecent');
+    
+    // Get the "Open Recently Used" editor
+    const openEditors = vscode.window.visibleTextEditors.filter(
+      editor => editor.document.uri.scheme === 'recentlyUsed'
+    );
+  
+    // Limit the number of recently used files to `maxHistory`
+    const recentFiles = openEditors.slice(0, maxHistory);
+  
+    // Show the quick pick menu to open the selected file
+    vscode.window.showQuickPick(recentFiles.map(editor => editor.document.fileName)).then(filePath => {
+      if (filePath) {
+        vscode.workspace.openTextDocument(filePath).then(doc => {
+          vscode.window.showTextDocument(doc);
+        });
+      }
+    });
+  }
+
+
+export function activate(context: ExtensionContext) {
+    context.subscriptions.push(vscode.commands.registerCommand('acsl-astra-c-extension.openRecentFiles', () => {
+        openRecentFiles();
+    }));
+}
+
